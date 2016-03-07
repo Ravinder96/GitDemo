ec805cc0437ef5cba7f74086b3d0f34ed60b23da
Ravinder <singhravinder943@gmail.com>
2016-03-07 18:12:20 +0530

    added welcome function

--- src/in/codekamp/gitdemo/Demo.java
+++ src/in/codekamp/gitdemo/Demo.java
@@ -1,7 +1,13 @@
 package in.codekamp.gitdemo;
 
+/**
+ * Created by hp1 on 07-03-2016.
+ */
 public class Demo {
     public static void main(String[] args) {
-
+           Demo.welcome();
+    }
+    public static void welcome(){
+        System.out.println("Welcome to codeKamp");
     }
 }
